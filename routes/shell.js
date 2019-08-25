3c3e3eb2388f751c41513bb9fc236eb0c2793d0d
peiili <1017935195@qq.com>
2019-08-25 22:32:26 +0800

    test

--- routes/shell.js
+++ routes/shell.js
@@ -5,22 +5,32 @@ const router = express.Router();
 shell.cd(__dirname);
 shell.cd("../")
 router.all("/push",function(req,res,next){  
-  if (!shell.which('git')) {
-    shell.echo('Sorry, this script requires git');
-    shell.exit(1);
-  }
-  console.log(req.body);
+  console.log(req.body.ref.split('/').pop());
   
-  // shell.exec("git fetch");
-  // shell.exec("git pull");
-  // shell.exec("cnpm install",{async:true});
-
+  // if(req.body.ref.split('/').pop()==='refs/heads/master'){
+  //   if (!shell.which('git')) {
+  //     shell.echo('Sorry, this script requires git');
+  //     shell.exit(1);
+  //   }
+  //   console.log(req.body);
+    
+  //   // shell.exec("git fetch");
+  //   // shell.exec("git pull");
+  //   // shell.exec("cnpm install",{async:true});
   
-  const data={
-    message:'pull OK'
-  }
-  res.send(data);
-  shell.config.reset(); 
+    
+  //   const data={
+  //     message:'pull OK'
+  //   }
+  //   res.send(data);
+  //   shell.config.reset(); 
+  // }else{
+  //   const data={
+  //     message:'master no pull'
+  //   };
+  //   res.send(data);
+  // }
+    res.send();
 })
 
 module.exports = router
