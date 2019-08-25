020d7494d447d0d594500922a724ca4cf369bbfe
peiili <1017935195@qq.com>
2019-08-25 17:25:28 +0800

    test auto commit

--- routes/shell.js
+++ routes/shell.js
@@ -5,21 +5,17 @@ const path = require("path")
 
 shell.cd(__dirname);
 shell.cd("../")
-const gitPath = shell.pwd(1).stdout;
 router.all("/push",function(req,res,next){  
   if (!shell.which('git')) {
     shell.echo('Sorry, this script requires git');
     shell.exit(1);
   }
-  shell.exec("git add .").code;
-  shell.exec("git commit -am 'autocommit'");
-  if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
-    shell.echo('Error: Git commit failed');
-    shell.exit(1);
+  // shell.exec("git fetch");
+  // shell.exec("git pull");
+  const data={
+    message:'pull OK'
   }
-  shell.exec("git push");
-  res.send("fwe");
-  // next();
+  res.send(data);
   shell.config.reset(); 
 })
 
