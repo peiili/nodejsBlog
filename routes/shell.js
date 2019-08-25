a6864273a230db47e29310d4d620fe39151100b5
peiili <1017935195@qq.com>
2019-08-25 22:36:59 +0800

    test

--- routes/shell.js
+++ routes/shell.js
@@ -5,31 +5,26 @@ const router = express.Router();
 shell.cd(__dirname);
 shell.cd("../")
 router.all("/push",function(req,res,next){  
-  console.log(req.body.ref.split('/').pop());
   
-  // if(req.body.ref.split('/').pop()==='refs/heads/master'){
-  //   if (!shell.which('git')) {
-  //     shell.echo('Sorry, this script requires git');
-  //     shell.exit(1);
-  //   }
-  //   console.log(req.body);
-    
-  //   // shell.exec("git fetch");
-  //   // shell.exec("git pull");
-  //   // shell.exec("cnpm install",{async:true});
-  
-    
-  //   const data={
-  //     message:'pull OK'
-  //   }
-  //   res.send(data);
-  //   shell.config.reset(); 
-  // }else{
-  //   const data={
-  //     message:'master no pull'
-  //   };
-  //   res.send(data);
-  // }
+  if(req.body&&req.body.ref.split('/').pop()==='refs/heads/master'){
+    if (!shell.which('git')) {
+      shell.echo('Sorry, this script requires git');
+      shell.exit(1);
+    }    
+    // shell.exec("git fetch");
+    // shell.exec("git pull");
+    // shell.exec("cnpm install",{async:true});
+    const data={
+      message:'pull OK'
+    }
+    res.send(data);
+    shell.config.reset(); 
+  }else{
+    const data={
+      message:'master no pull'
+    };
+    res.send(data);
+  }
     res.send();
 })
 
